4aaa0a3834bb3dabb07b1a848e4f7fe8a19ff6f7
samis <samilsully@gmail.com>
2020-10-08 12:45:18 -0700

    Added basics to handlebars files to get something to display when deployed to heroku

---
+++ server.js
@@ -0,0 +1,28 @@
+
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql");
+
+const app = express();
+var PORT = process.env.PORT || 8080;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res)=> {
+    res.render("index");
+})
+
+app.get("/api/config", (req, res) => {
+    res.json({
+        success: true,
+    })
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
