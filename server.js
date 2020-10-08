33a6ac0fa5ec03bab7bc18becb7bb406224f70df
samis <samilsully@gmail.com>
2020-10-08 13:54:46 -0700

    copied data from cats activity in class to build from

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 
 const express = require("express");
 const exphbs = require("express-handlebars");
-const mysql = require("mysql");
 
 const app = express();
 var PORT = process.env.PORT || 8080;
 
+app.use(express.static("public"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
 
 app.get("/", (req, res)=> {
     res.render("index");
